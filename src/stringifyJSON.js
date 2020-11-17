e44085919eb43ebe3780556b5a217f53bcd6ce45
rawcliffeisaac <rawcliffe.isaac@gmail.com>
2020-11-16 16:11:13 -0800

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,39 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // can't handle null or undefined
+  if (obj === null) {
+    return 'null';
+  }
+  if (obj === undefined) {
+    return 'undefined';
+  }
+  // check if obj is string
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  // check if obj is array
+  if (Array.isArray(obj)) {
+    // create result array
+    let array = [];
+    // iterate through array
+    // array = array.push(obj.forEach(stringifyJSON));
+    for (var i = 0; i < obj.length; i++) {
+      array.push(stringifyJSON(obj[i]));
+    }
+    return '[' + array.toString() + ']';
+  }
+  // check if obj is object
+  if (typeof obj === 'object') {
+    // create result object
+    let array = [];
+    for (item in obj) {
+      if (obj[item] !== undefined && typeof obj[item] !== 'function') {
+        array.push(stringifyJSON(item) + ':' + stringifyJSON(obj[item]));
+      }
+    }
+    return '{' + array.toString() + '}';
+  }
+
+  return obj.toString();
 };
