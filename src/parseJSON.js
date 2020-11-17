0ee56905146cc5d84c93170278dd6a12db0d54ff
rawcliffeisaac <rawcliffe.isaac@gmail.com>
2020-11-16 17:18:59 -0800

    Started on parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,32 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  // create a memory
+  // look at each individual character
+  // create for housing characters
+  // iterate over each character in jsonstring
+  // if character is \
+  // special instance
+  // if character is a { or [ then recursion is called
+  // if character is } or ] then return previous characters since the last bracket
+  for (let i = 0; i < json.length; i++) {
+    if (json[i] === '{') {
+      let memory = {};
+      if (json[i + 1] === '}') {
+        return memory;
+      }
+    // memory parseJSON(json[i + 1]);
+    }
+    if (json[i] === '[') {
+    // handle arrays
+      let memory = [];
+      if (json[i + 1] === '}') {
+        return memory;
+      }
+    }
+    if (json[i] === '"') {
+      // handle string
+    }
+  }
+
 };
